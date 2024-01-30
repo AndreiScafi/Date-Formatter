7ff3b921c567ab8fa33181edb2a597f752bae9b9
Andrei Scafi <andreiscafi@hotmail.com>
2024-01-30 09:58:55 -0300

    Detecting changes in date format with Javascript.

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ const minutes = date.getMinutes();
 
 const formattedDate = `${day}-${month}-${year}`;
 // End of Variables
+
+// Displaying the date
+currentDateParagraph.textContent = formattedDate;
+// End of Displaying the date
+
+// Detecting changes in date
+dateOptionsSelectElement.addEventListener('change', () => {
+    switch (dateOptionsSelectElement.value) {
+        case "yyyy-mm-dd":
+            currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-");
+            break;
+        case "mm-dd-yyyy-h-mm":
+            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+            break;
+        default:
+            currentDateParagraph.textContent = formattedDate;
+            break;
+    }
+})
+// End of Detecting changes in date
+
+
